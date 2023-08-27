698dd54e710ef1a4e656977f53c0b72967a0567c
EricTruchot <96569281+EricTruchot@users.noreply.github.com>
2023-08-27 11:01:44 +0200

    test users from bdd.js

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 // export const users = { user: "USER" }
+import bdd from './bdd.js'
 const express = require('express');
 
 const router = express.Router();
@@ -6,6 +7,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.json(['😀','user2', '😳', '🙄']);
 });
+router.get('/user', (req, res) => {
+    res.json(bdd.users[1]);
+  });
 router
   .route("/:id")
   .get((req, res) => {
